ed04d5b06dbf28974a693dce76c16a1621f2f3a1
Zacc Elliott <113382070+AdminZacc@users.noreply.github.com>
2024-01-04 14:22:43 -0500

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,51 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+const element = document.querySelectorAll(".element");
+function PageTransitions() {
+  // Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+
+      //Check if currentBtn is defined before accessing its properties
+      if (currentBtn[0]) {
+        currentBtn[0].className = currentBtn[0].className.replace(
+          "active-btn",
+          ""
+        );
+      }
+
+      sectBtn[i].classList.add("active-btn");
+    });
+  }
+
+  // Sections active class
+  for (let i = 0; i < allSections.length; i++) {
+    allSections[i].addEventListener("click", (e) => {
+      const id = e.target.dataset.id;
+
+      if (id) {
+        // remove selected from other buttons
+        sectBtns.forEach((btn) => {
+          btn.classList.remove("active");
+        });
+
+        e.target.classList.add("active");
+
+        // hide other sections
+        sections.forEach((section) => {
+          section.classList.remove("active");
+        });
+
+        const element = document.getElementById(id);
+        // Check if the element with the given ID exists
+        if (element) {
+          element.classList.add("active");
+        }
+      }
+    });
+  }
+}
+PageTransitions();
