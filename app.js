2a8804f604f0f23e81d0655a9ec7090d25d21948
Zacc <zacelliott17@gmail.com>
2024-02-27 05:40:15 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const sectBtn = document.querySelectorAll(".control");
-const allSections = document.querySelectorAll(".main-content")[0];
-const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".controls .control");
-
-sectBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    console.log("Button clicked"); // Check if the button click event is triggered
-    const id = btn.getAttribute("data-id");
-    console.log("ID:", id); // Check the retrieved ID
-
-    // Toggle active button
-    sectBtns.forEach((button) => {
-      button.classList.remove("active-btn");
-    });
-    btn.classList.add("active-btn");
-
-    // Show the selected section
-    sections.forEach((section) => {
-      section.classList.remove("active");
-    });
-    const element = document.getElementById(id);
-    console.log("Element:", element); // Check if the correct element is selected
-    if (element) {
-      element.classList.add("active");
-    }
+(function () {
+  [...document.querySelectorAll(".control")].forEach(button => {
+      button.addEventListener("click", function() {
+          document.querySelector(".active-btn").classList.remove("active-btn");
+          this.classList.add("active-btn");
+          document.querySelector(".active").classList.remove("active");
+          document.getElementById(button.dataset.id).classList.add("active");
+      })
   });
-});
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
+  })
+})();
